6d3176cadcef9e64befecfc0ab64ea922939cbed
Peter Kong <bansuki@gmail.com>
2017-11-29 20:18:59 -0600

    need to implement saving

--- src/components/SvgNavItem.js
+++ src/components/SvgNavItem.js
@@ -4,10 +4,13 @@ import SVGInline from "react-svg-inline";
 
 class SavedNav extends Component {
   render() {
+    let width = this.props.isCurrent ? "60" : "40";
+    let height = this.props.isCurrent ? "40" : "30";
     return (
       <div className="SavedNav">
-        <SVGInline svg={this.props.svgItem.svg}
-          onClick={e => this.props._handleSelect(this.props.svgItem)}/>
+        <SVGInline svg={this.props.svgItem.svg} width={width} height={height}
+          onClick={e => this.props._handleTimeTravel(this.props.currentSvg, this.props.params)}/>
+        <span>{JSON.stringify(this.props.params)}</span>
       </div>
     );
   }
