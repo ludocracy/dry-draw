db438089ed0afb1eee95d237d2dfe77e43e79145
Peter Kong <bansuki@gmail.com>
2017-12-12 21:25:29 -0600

    fixed it - also got rid of uncontrolled input warning

--- src/components/designer/panels/IfPanel.js
+++ src/components/designer/panels/IfPanel.js
@@ -5,13 +5,32 @@ import PropertyGroup from './PropertyGroup';
 import Columns from './Columns';
 
 export default class IfPanel extends Panel {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: this.props.object.if || ''
+    }
+
+    this.onChange = this.onChange.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({
+      value: e.target.value
+    }, () => (
+      this.props.onChange('if', this.state.value)
+    ));
+  }
+
   render() {
     let {object} = this.props;
+    let value = object.if || '';
     return (
       <PropertyGroup object={object}>
         <Columns label="Exists if">
-          <input type="text" value={object.if}
-            onChange={e => this.props.onChange('if', e.target.value)} />
+          <input type="text" value={value}
+            onChange={this.onChange} />
         </Columns>
       </PropertyGroup>
     );
